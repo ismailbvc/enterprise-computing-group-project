1c918659de4d830d926a81b8dec623416e537c5b
elhardoum <elhardoum@outlook.com>
2022-04-11 11:35:28 -0600

    sample express app

---
+++ server.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.send('hello world!'))
+
+const server = app.listen(80, () => console.log(`Server listening on localhost:${server.address().port}`))
