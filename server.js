06e5a9675423d0642b25e29037495956ed0dfdc4
elhardoum <elhardoum@outlook.com>
2022-04-13 11:55:39 -0600

    added sample page with handlebars.js template engine

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import hbs from 'hbs'
+import path from 'path'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('hello world!!'))
+const __dirname = path.resolve()
+
+app.set('view engine', 'html')
+app.engine('html', hbs.__express)
+app.set('views', `${__dirname}/src/views`)
+
+app.get('/', (req, res) => res.render('home'))
 
 const server = app.listen(80, () => console.log(`Server listening on localhost:${server.address().port}`))
